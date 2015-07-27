97604a66a1fd95c6ae31069ebe14ce6ea8fdfdb1
Martin-Zack Mekkaoui <martin@mekkaoui.fr>
2015-07-27 20:20:00 +0200

    moar endpoints covered

--- bright/bright.py
+++ bright/bright.py
@@ -7,7 +7,7 @@ from .helpers import raise_errors_on_failure
 
 class Bright(object):
     use_ssl = True
-    api_url = "http://api.brightfor.me/v1/"
+    api_url = "https://api.brightfor.me/v1/"
     host = "api.brightfor.me"
     __version__ = '0.0.1'
     USER_AGENT = 'BRIGHT Python API Wrapper {0}'.format(__version__)
@@ -45,27 +45,74 @@ class Bright(object):
                                                 scope=self.scopes,
                                                 )
 
-
-        
-
-    def make_call(self,endpoint,method,payload={}):
+    def make_request(self,endpoint,method,payload={},params={}):
         payload = json.dumps(payload)
         headers = {
             'User-Agent': self.USER_AGENT,
             'Accept':'application/json'
         }
         if method == 'GET':
-            r = self.bright.get(self.api_url + endpoint,headers=headers)
+            r = self.bright.get(self.api_url + endpoint, 
+                                params=params,
+                                headers=headers)
             r = raise_errors_on_failure(r)
         if method == 'DELETE':
-            r = self.bright.delete(self.api_url + endpoint, data=payload, headers=headers)
+            r = self.bright.delete(self.api_url + endpoint, 
+                                    params=params, 
+                                    data=payload, 
+                                    headers=headers)
             r = raise_errors_on_failure(r)
         if method == 'POST':
-            r = self.bright.post(self.api_url + endpoint, data=payload, headers=headers)
+            r = self.bright.post(self.api_url + endpoint, 
+                                params=params, 
+                                data=payload, 
+                                headers=headers)
             r = raise_errors_on_failure(r)
         if method == 'PUT':
-            r = self.bright.put(self.api_url + endpoint, data=payload, headers=headers)
+            r = self.bright.put(self.api_url + endpoint, 
+                                params=params, 
+                                data=payload, 
+                                headers=headers)
             r = raise_errors_on_failure(r)
 
         return r.json()
+
+    def me(self):
+        return self.make_request('me','GET')
+
+    def me_collections(self):
+        return self.make_request('me/collections','GET')
+
+    def me_artworks(self):
+        return self.make_request('me/artworks','GET')
+
+    def me_notifications(self):
+        return self.make_request('me/notifications','GET')
+
+    def update_me(self, data={}):
+        return self.make_request('me','PUT',payload=data)
+
+    def get_artwork(self, artwork_id):
+        uri = "artworks/{0}".format(artwork_id)
+        return self.make_request(uri,'GET')
+
+    def get_all_artworks(self,per_page=None,page=None):
+
+        params = {
+            "page":page,
+            "per_page": per_page or 10
+            }
+
+        return self.make_request('artworks/','GET', params=params)
+
+    def update_artwork(self,artwork_id, data={}):
+        uri = 'artworks/{0}'.format(artwork_id)
+        return self.make_request(uri,'PUT',payload=data)
+
+    def delete_artwork(self,artwork_id):
+        uri = 'artworks/{0}'.format(artwork_id)
+        return self.make_request(uri,'DELETE')
+
+
+
     
