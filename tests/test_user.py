6d2a903b6fc0aa19fc1048ddb5ad5ee3af0e262e
hellerve <veit.heller@student.htw-berlin.de>
2015-12-28 15:46:08 +0100

    moved tests to class

--- tests/test_user.py
+++ tests/test_user.py
@@ -3,49 +3,50 @@ import unittest
 import bright
 import settings
 
-bright_api = bright.Bright(client_id=settings.client_id,
-                           client_secret=settings.client_secret,
-                           **settings.kwargs)
+from bright.helpers import Forbidden, ResourceNotFound
 
-def test_get_me():
-    "Test we can get our own profile"
-    contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
-                "accounts", "user_type", "counts"]
-    res = bright_api.me()
-    unittest.assertIn("user", res)
 
-    for element in contents:
-        unittest.assertIn(element, res["user"])
+class UserTests(unittest.TestCase):
 
-def test_get_user():
-    "Test that we can get another users profile"
-    contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
-                "accounts", "user_type", "counts"]
-    res = bright_api.get_user("test_user")
-    unittest.assertIn("user", res)
+    @classmethod
+    def setupClass(self):
+        self.bright_api = bright.Bright(client_id=settings.client_id,
+                                        client_secret=settings.client_secret,
+                                        **settings.kwargs)
 
-    for element in contents:
-        unittest.assertIn(element, res["user"])
+    def test_get_me(self):
+        "Test we can get our own profile"
+        contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
+                    "accounts", "user_type", "counts"]
+        res = self.bright_api.me()
+        self.assertIn("user", res)
 
-def test_get_user_404():
-    "Test that we get an exception if the user does not exist"
-    with unittest.assertRaises(bright.ResourceNotFound):
-        res = bright_api.get_user("this_user_does_not_exist")
+        for element in contents:
+            self.assertIn(element, res["user"])
 
-def test_update_me():
-    "Tes that we can update ourselves"
-    data = {
-      "screenname": "foobar"
-    }
-    res = bright_api.update_me(data)
-    unittest.assertIn("user", res)
-    unittest.assertIn("screenname", res["user"])
-    unittest.assertEquals(data["screenname"], res["user"]["screenname"])
+    def test_get_user(self):
+        "Test that we can get another users profile"
+        contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
+                    "accounts", "user_type", "counts"]
+        res = self.bright_api.get_user("test_user")
+        self.assertIn("user", res)
 
-def test_update_user_403():
-    "Tes that we cannot update other users"
-    data = {
-      "screenname": "foobar"
-    }
-    with unittest.assertRaises(bright.Forbidden):
-        res = bright_api.update_user("test_user", data)
+        for element in contents:
+            self.assertIn(element, res["user"])
+
+    def test_get_user_404(self):
+        "Test that we get an exception if the user does not exist"
+        with self.assertRaises(ResourceNotFound):
+            res = self.bright_api.get_user("this_user_does_not_exist")
+
+    def test_update_me(self):
+        "Tes that we can update ourselves"
+        data = {
+          "user": {
+            "screenname": "foobar"
+          }
+        }
+        res = self.bright_api.update_me(data)
+        self.assertIn("user", res)
+        self.assertIn("screenname", res["user"])
+        self.assertEquals(data["screenname"], res["user"]["screenname"])
