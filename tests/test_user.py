576dfdb1ec0b8c3d8b010e24cf5d8eb4fc09ffd5
hellerve <veit.heller@student.htw-berlin.de>
2015-12-28 11:56:46 +0100

    further skeleton (fixtures needed?)

--- tests/test_user.py
+++ tests/test_user.py
@@ -8,7 +8,7 @@ bright_api = bright.Bright(client_id=settings.client_id,
                            **settings.kwargs)
 
 def test_get_me():
-    "Test we can get a user"
+    "Test we can get our own profile"
     contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
                 "accounts", "user_type", "counts"]
     res = bright_api.me()
@@ -16,3 +16,18 @@ def test_get_me():
 
     for element in contents:
         unittest.assertIn(res["user"], element)
+
+def test_get_user():
+    "Test that we can get another users profile"
+    contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
+                "accounts", "user_type", "counts"]
+    res = bright_api.get_user("test_user")
+    unittest.assertIn(res, "user")
+
+    for element in contents:
+        unittest.assertIn(res["user"], element)
+
+def test_get_user_404():
+    "test that we get an exception if the user does not exist"
+    with unittest.assertRaises(bright.ResourceNotFound):
+        res = bright_api.get_user("this_user_does_not_exist")
