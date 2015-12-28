65fd6a70055b5b10977ada049b3a064513d538e5
hellerve <veit.heller@student.htw-berlin.de>
2015-12-28 12:07:05 +0100

    fixed assertIn (I always get those wrong)

--- tests/test_user.py
+++ tests/test_user.py
@@ -12,22 +12,32 @@ def test_get_me():
     contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
                 "accounts", "user_type", "counts"]
     res = bright_api.me()
-    unittest.assertIn(res, "user")
+    unittest.assertIn("user", res)
 
     for element in contents:
-        unittest.assertIn(res["user"], element)
+        unittest.assertIn(element, res["user"])
 
 def test_get_user():
     "Test that we can get another users profile"
     contents = ["cover_url", "picture_url", "fullname", "screenname", "bio",
                 "accounts", "user_type", "counts"]
     res = bright_api.get_user("test_user")
-    unittest.assertIn(res, "user")
+    unittest.assertIn("user", res)
 
     for element in contents:
-        unittest.assertIn(res["user"], element)
+        unittest.assertIn(element, res["user"])
 
 def test_get_user_404():
-    "test that we get an exception if the user does not exist"
+    "Test that we get an exception if the user does not exist"
     with unittest.assertRaises(bright.ResourceNotFound):
         res = bright_api.get_user("this_user_does_not_exist")
+
+def test_update_me():
+    "Tes that we can update ourselves"
+    data = {
+      "screenname": "foobar"
+    }
+    res = bright_api.update_me(data)
+    unittest.assertIn("user", res)
+    unittest.assertIn("screenname", res["user"])
+    unittest.assertEquals(data["screenname"], res["user"]["screenname"])
